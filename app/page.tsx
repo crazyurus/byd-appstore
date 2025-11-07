62c1b5e172a5782ff1c77aa3a09b0671fe09e8fc
Cr4zy Uru5 <crazyurus@vip.qq.com>
2025-11-07 18:46:22 +0800

    refactor: 位置调整

--- app/page.tsx
+++ app/page.tsx
@@ -32,16 +32,12 @@ async function AppStore(props: Props): Promise<JSX.Element> {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-4xl flex-col p-16 bg-white dark:bg-black sm:items-start">
-        <div className="mb-4">
-          <SelectPlatform platform={platform} />
-        </div>
-        <div>
-          <Toggle category={category} />
-        </div>
-        <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl my-10">
+        <Toggle category={category} />
+        <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl mt-10 mb-4">
           {title}
         </h1>
-        <ItemGroup className="w-full gap-6">
+        <SelectPlatform platform={platform} />
+        <ItemGroup className="mt-4 w-full gap-6">
           {apps.map(item => (
             <Item key={item.id} variant="outline">
               <ItemMedia>
